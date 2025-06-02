0a0a3d415e233eb290351092746ce077698fd393
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-02 17:15:20 -0400

    adding code for second player

--- body.js
+++ body.js
@@ -1,20 +1,43 @@
 let body = document.querySelector('body');
 body.style.display = "flex";
+let button1 = document.createElement('button');
+let input1 = document.createElement("input");
+let label = document.createElement('label');
+
+
+
+button1.setAttribute("id", 'btn1');
+button1.innerHTML = "<h3>Get Data</h3>";
+input1.setAttribute("id","input1");
+input1.innerText = "Type Here";
+
+body.appendChild(input1);
+body.appendChild(button1);
+
+button1.addEventListener('click', text1);
+
+function text1(){
+    console.log("clicked1")
+}
+
+
 let button = document.createElement('button');
 let input = document.createElement("input");
 
-button.setAttribute("id", 'btn');
-button.innerHTML = "<h3>Get Data</h3>";
-input.setAttribute("id","input");
+button.setAttribute("id", 'btn2');
+button.innerHTML = "<h3>Random Data</h3>";
+input.setAttribute("id","input2");
 input.innerText = "Type Here";
 
 body.appendChild(input);
 body.appendChild(button);
 
-button.addEventListener('click', text);
+button.addEventListener('click', text2);
 
-function text(){
-    console.log("clicked")
+function text2(){
+    console.log("clicked2")
 }
+body.appendChild(label)
+label.innerText = 'Who Will Win?'
 
 // export {button}
--- fetch.js
+++ fetch.js
@@ -1,18 +1,40 @@
 // import {button} from "./body.js"
+let myData1;
+let myData2;
+
 let object1 = fetch("https://dragonball-api.com/api/characters/2")
 .then(response => response.json())
-.then(data => console.log(data, data.name, data.ki)) 
+.then(data1 => {
+    myData1 = data1.ki;
+    
+    console.log(data1)
+    console.log(data1.name)
+    console.log(data1.ki)
+
+
+}) 
 
 
 let object2 = fetch("https://dragonball-api.com/api/characters/1")//ranodm generator
 .then(response => response.json())
-.then(data => console.log(data, data.name, data.ki))
+.then(data2 => {
+    myData2 = data2.ki;
+   
+    console.log(data2)
+    console.log(data2.name)
+    console.log(data2.ki)
+})
 .catch(error => console.error(error));
  //player1
 //fetch()//player2
 
 //console.log(object)
+ if(myData1 > myData2){
+    console.log(`You Win! ${myData2} Loses!`)
 
+}else if(myData2> myData1){
+    console.log(`You Lose! ${myData2} Wins!`)
+}
 object1
 object2
 
