85e9fc40f4b08968754669f68cb78c05816f1b05
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-06-02 13:49:58 -0400

    just added my input and button

---
+++ body.js
@@ -0,0 +1,13 @@
+let body = document.querySelector('body');
+body.style.display = "flex";
+let button = document.createElement('button');
+let input = document.createElement("input");
+
+button.setAttribute("id", 'btn');
+button.innerHTML = "<h3>Get Data</h3>";
+input.setAttribute("id","input");
+input.innerText = "Type Here";
+
+body.appendChild(input);
+body.appendChild(button);
+
